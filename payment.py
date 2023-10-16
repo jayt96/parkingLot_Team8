ebc9c6059793752ff102ab51991ee041a1c96d70
jayt96 <147595415+jayt96@users.noreply.github.com>
2023-10-16 17:14:12 +0900

    Update payment.py

--- payment.py
+++ payment.py
@@ -15,20 +15,18 @@ class Payment:
                 print("Invalid input. Please enter a valid payment amount.")
                 continue
             if payment_input > amount:
-                print("Payment successful. Charge: {:.2f}".format(payment_input - amount))
+                print("Payment successful. Change: {:.2f}".format(payment_input - amount))
                 account.account_status = AccountStatus.SUBSCRIBED
                 conn = sqlite3.connect("AccountDB.db")
                 cursor = conn.cursor()
-                cursor.execute("UPDATE accounts SET account_status = ? WHERE login = ?",
-                               (account.account_status.value, account.login))
+                cursor.execute(f"UPDATE accounts SET accountType = 4 WHERE login = '{account.login}'")
                 break
             elif payment_input == amount:
-                print("Payment successful. No charge.")
+                print("Payment successful. No change.")
                 account.account_status = AccountStatus.SUBSCRIBED
                 conn = sqlite3.connect("AccountDB.db")
                 cursor = conn.cursor()
-                cursor.execute("UPDATE accounts SET accountType = ? WHERE login = ?",
-                               (4, account.login()))
+                cursor.execute(f"UPDATE accounts SET accountType = 4 WHERE login = '{account.login}'")
                 conn.commit()
                 break
             else:
